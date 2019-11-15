0e9952a3192d4502fe4641693b212d9e653ba0aa
Dilyen <davidandohacquah@gmail.com>
2019-11-15 17:06:21 +0000

    added integration test

---
+++ src/test/java/io/turntabl/Dbtest.java
@@ -0,0 +1,78 @@
+package io.turntabl;
+
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class Dbtest {
+
+    public static Optional<Connection> getConnection() {
+        try{
+            Class.forName("org.h2.Driver");
+            String url = "jdbc:h2:~/Desktop/management_system/test";
+            try {
+                return Optional.ofNullable(DriverManager.getConnection(url, "", ""));
+            }catch (SQLException e){
+                e.printStackTrace();
+                System.out.println("Connection  Error: " + e.getMessage());
+                return Optional.empty();
+            }
+        }catch (ClassNotFoundException e ){
+            System.out.println("Driver Class Error: " + e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+
+    public static void createDb() throws SQLException {
+
+        Connection conn = getConnection().get();
+        Statement statement = conn.createStatement();
+        statement.execute("create table clients(client_id int primary key, client_name text not null, client_address text not null, client_telephone text not null, client_email text not null)");
+        statement.execute("insert into clients values(1, 'David Ameyaw', 'Lapaz', '0558314855', 'davidameyaw@gmail.com')");
+        statement.execute("insert into clients values(2, 'Doreen Amankwa', 'Accra', '0505092133', 'doreen@gmail.com')");
+        statement.execute("insert into clients values(3, 'Kwakye Osei', 'Capecoast', '0549163989', 'oseikwakye@gmail.com')");
+        statement.execute("insert into clients values(4, 'Alex owusu', 'Achimota', '023654786', 'alexowusu@gmail.com')");
+    }
+
+    @Test
+    public void viewClients() throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        String url = "jdbc:h2:~/Desktop/management_system/test";
+        List<String> client_details = new ArrayList<>();
+        Connection conn = DriverManager.getConnection(url, "", "");
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("select * from clients");
+        while (rs.next()) {
+            client_details.add(rs.getString("client_name"));
+            client_details.add(rs.getString("client_address"));
+        }
+        List<String> details = Arrays.asList("David Ameyaw", "Lapaz", "Doreen Amankwa", "Accra", "Kwakye Osei", "Capecoast", "Alex owusu", "Achimota");
+        assertEquals(details, client_details);
+
+    }
+
+    @Test
+    public void countRows() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        String url = "jdbc:h2:~/Desktop/management_system/test";
+        Connection conn = DriverManager.getConnection(url, "", "");
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("select * from clients");
+        int count = 0;
+        while (rs.next()) {
+            count += 1;
+        }
+        int expected = 4;
+        assertEquals(expected, count);
+
+    }
+
+
+}
